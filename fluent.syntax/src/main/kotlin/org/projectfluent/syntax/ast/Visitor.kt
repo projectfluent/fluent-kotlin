041823a2ec3ecee9fa7d721a803cf99de3b436dd
Axel Hecht <axel@pike.org>
2020-06-04 17:09:29 +0200

    Add a Visitor pattern as abstract base class (#3)

---
+++ fluent.syntax/src/main/kotlin/org/projectfluent/syntax/ast/Visitor.kt
@@ -0,0 +1,37 @@
+package org.projectfluent.syntax.ast
+
+import kotlin.reflect.KFunction
+import kotlin.reflect.KVisibility
+import kotlin.reflect.full.memberFunctions
+import kotlin.reflect.full.memberProperties
+
+abstract class Visitor {
+    val handlers: MutableMap<String, KFunction<*>> = mutableMapOf()
+    init {
+        this::class.memberFunctions.filter {
+            it.name.startsWith("visit_")
+        }.map {
+            handlers[it.name.substring("visit_".length)] = it
+        }
+    }
+    fun visit(node: BaseNode) {
+        val cName = node::class.simpleName
+        val handler = this.handlers[cName]
+        if (handler != null) {
+            val foo = handler.call(this, node)
+        } else {
+            this.generic_visit(node)
+        }
+    }
+    fun generic_visit(node: BaseNode) {
+        node::class.memberProperties.forEach {
+            if (it.visibility == KVisibility.PUBLIC) {
+                val value = it.getter.call(node)
+                when (value) {
+                    is BaseNode -> this.visit(value)
+                    is Collection<*> -> value.filterIsInstance<BaseNode>().map { this.visit(it) }
+                }
+            }
+        }
+    }
+}
