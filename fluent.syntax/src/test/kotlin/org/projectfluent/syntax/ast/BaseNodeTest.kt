2fc610dcb1c65014249db35713671dec2e0b2b69
Michal Stanke <michal@stanke.cz>
2020-06-23 11:44:42 +0200

    Re-implement equals semantics for AST nodes
    - replace custom nodeEquals method by overloading equals
    - implement equals with ignoredFields using Kotlin collection operations
    - reduce reflection calls
    - avoid nullability in equals
    - fix equals semantics for BaseNode children (data classes have implicit equals with other than expected behaviour)

--- fluent.syntax/src/test/kotlin/org/projectfluent/syntax/ast/BaseNodeTest.kt
+++ fluent.syntax/src/test/kotlin/org/projectfluent/syntax/ast/BaseNodeTest.kt
@@ -5,61 +5,64 @@ import org.junit.jupiter.api.Test
 import org.projectfluent.syntax.parser.FluentParser
 
 class BaseNodeTest {
-    val parser = FluentParser()
-    @Test fun test_equals() {
+    private val parser = FluentParser()
+
+    @Test
+    fun testEquals() {
         val m1 = Message(Identifier("test-id"), Pattern(TextElement("localized")))
-        assertTrue(m1.nodeEquals(m1))
         val m2 = Message(Identifier("test-id"), Pattern(TextElement("different")))
-        assertFalse(m1.nodeEquals(m2))
-        assertTrue(m1.nodeEquals(m2, arrayOf("span", "value")))
+
+        assertEquals(m1, m1)
         assertNotEquals(m1, m2)
         assertEquals(m1.id, m2.id)
-        assertFalse(m1.id.nodeEquals(m2.value))
-        assertFalse(m1.nodeEquals(null))
+        assertNotEquals(m1.value, m2.value)
         assertNotEquals(m1, null)
         assertNotEquals(null, m1)
     }
 
     @Test
-    fun variant_order() {
-        val thisRes = this.parser.parse(
-            """
-            |msg = { ${'$'}val ->
-            |  [few] things
-            |  [1] one
-            | *[other] default
-            |}
-        """.trimMargin()
-        )
-        val otherRes = this.parser.parse(
-            """
-            |msg = { ${'$'}val ->
-            |  [few] things
-            | *[other] default
-            |  [1] one
-            |}
-        """.trimMargin()
-        )
-        assertTrue(thisRes.body[0].nodeEquals(otherRes.body[0], ignoredFields = arrayOf("span", "variants")))
-        assertFalse(thisRes.body[0].nodeEquals(otherRes.body[0]))
+    fun testEqualsWithIgnoredFields() {
+        val m1 = Message(Identifier("test-id"), Pattern(TextElement("localized")))
+        val m2 = Message(Identifier("test-id"), Pattern(TextElement("different")))
+
+        assertTrue(m1.equals(m2, setOf("span", "value")))
     }
+
     @Test
-    fun attribute_order() {
-        val thisRes = this.parser.parse(
-            """
+    fun variantOrderIsImportantForEquals() {
+        val resource1 = this.parser.parse("""
+            |msg = { ${'$'}val ->
+            |  [few] things
+            |  [1] one
+            | *[other] default
+            |}
+        """.trimMargin())
+        val resource2 = this.parser.parse("""
+            |msg = { ${'$'}val ->
+            |  [few] things
+            | *[other] default
+            |  [1] one
+            |}
+        """.trimMargin())
+
+        assertTrue(resource1.body[0].equals(resource2.body[0], ignoredFields = setOf("span", "variants")))
+        assertNotEquals(resource1.body[0], resource2.body[0])
+    }
+
+    @Test
+    fun attributeOrderIsImportantForEquals() {
+        val resource1 = this.parser.parse("""
             |msg =
             |  .attr1 = one
             |  .attr2 = two
-        """.trimMargin()
-        )
-        val otherRes = this.parser.parse(
-            """
+        """.trimMargin())
+        val resource2 = this.parser.parse("""
             |msg =
             |  .attr2 = two
             |  .attr1 = one
-        """.trimMargin()
-        )
-        assertTrue(thisRes.body[0].nodeEquals(otherRes.body[0], ignoredFields = arrayOf("span", "attributes")))
-        assertFalse(thisRes.body[0].nodeEquals(otherRes.body[0]))
+        """.trimMargin())
+
+        assertTrue(resource1.body[0].equals(resource2.body[0], ignoredFields = setOf("span", "attributes")))
+        assertNotEquals(resource1.body[0], resource2.body[0])
     }
 }
