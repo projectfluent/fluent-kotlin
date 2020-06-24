affbec5272529d2d0374d31e2c210f41153bcf15
Michal Stanke <michal@stanke.cz>
2020-06-24 18:48:23 +0200

    Check visit method name matches the accepted BaseNode class

--- fluent.syntax/src/main/kotlin/org/projectfluent/syntax/visitor/Visitor.kt
+++ fluent.syntax/src/main/kotlin/org/projectfluent/syntax/visitor/Visitor.kt
@@ -1,10 +1,13 @@
 package org.projectfluent.syntax.visitor
 
 import org.projectfluent.syntax.ast.BaseNode
+import java.io.InvalidClassException
 import java.util.concurrent.ConcurrentHashMap
 import kotlin.reflect.KClass
+import kotlin.reflect.KVisibility
 import kotlin.reflect.full.isSubclassOf
 import kotlin.reflect.jvm.jvmName
+import kotlin.reflect.jvm.kotlinFunction
 
 /**
  * Generic Visitor base class.
@@ -13,6 +16,10 @@ import kotlin.reflect.jvm.jvmName
  * e.g. `visitResource(node: Resource)` to handle the `Resource` node type.
  */
 abstract class Visitor {
+
+    init {
+        handlers(this::class)
+    }
 
     /**
      * Primary entry point for all visitors.
@@ -49,8 +56,17 @@ abstract class Visitor {
                 clazz.jvmName,
                 {
                     clazz.java.declaredMethods
-                        .filter { it.name.startsWith("visit") }
+                        .filter { it.name.startsWith("visit") && it.kotlinFunction?.visibility == KVisibility.PUBLIC }
                         .filter { it.parameterCount == 1 && it.parameterTypes[0].kotlin.isSubclassOf(BaseNode::class) }
+                        .filter { method ->
+                            val argumentClassName = method.parameterTypes[0].kotlin.simpleName
+                            val expectedMethodName = "visit$argumentClassName"
+                            if (method.name == expectedMethodName) {
+                                true
+                            } else {
+                                throw InvalidClassException("Method '${clazz.simpleName}.${method.name}($argumentClassName)' must be called '$expectedMethodName', or must not be public.")
+                            }
+                        }
                         .associate {
                             Pair(
                                 it.parameterTypes[0].kotlin.jvmName,
--- fluent.syntax/src/test/kotlin/org/projectfluent/syntax/visitor/VisitorTest.kt
+++ fluent.syntax/src/test/kotlin/org/projectfluent/syntax/visitor/VisitorTest.kt
@@ -1,8 +1,11 @@
 package org.projectfluent.syntax.visitor
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
+import org.projectfluent.syntax.ast.BaseNode
 import org.projectfluent.syntax.parser.FluentParser
+import java.io.InvalidClassException
 
 class VisitorTest {
     val parser = FluentParser()
@@ -23,5 +26,25 @@ class VisitorTest {
         assertEquals(3, visitor.wordCount)
         assertEquals(2, visitor.patternCount)
         assertEquals(1, visitor.variantCount)
+    }
+
+    @Test
+    fun validatesPublicVisitMethodNames() {
+        class InvalidVisitor : Visitor() {
+            fun visitFoo(node: BaseNode) = run { }
+        }
+
+        assertThrows(InvalidClassException::class.java) {
+            InvalidVisitor()
+        }
+    }
+
+    @Test
+    fun ignoresPrivateVisitMethodNames() {
+        class ValidVisitor : Visitor() {
+            private fun visitFoo(node: BaseNode) = run { }
+        }
+
+        ValidVisitor()
     }
 }
